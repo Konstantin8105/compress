8580a6bb513cae84c64ad245df21ffb44ec26b93
Konstantin8105 <Konstantin8105@gmail.com>
2024-07-26 10:23:49 +0300

    init

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// command
+	// ffmpeg.exe -i input.mp4 -vf scale=640:-1 out.mp4
+	ffmpegLocation := "C:\\Users\\e19700019\\Downloads\\ffmpeg\\bin\\ffmpeg.exe"
+	folder := "O:\\NLP\\"
+}
